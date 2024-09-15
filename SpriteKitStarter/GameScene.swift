f06ef5f60d55c76835e9e1f6e321583a5d489481
Hollister009 <reactive.andrew@yahoo.com.ua>
2024-09-15 14:45:54 +0300

    Implement dragging the square and update recalculateLayout method

--- SpriteKitStarter/GameScene.swift
+++ SpriteKitStarter/GameScene.swift
@@ -12,10 +12,12 @@ class GameScene: SKScene {
     
     private var square: SKSpriteNode!
     private var ground: SKSpriteNode!
+    private var isDraggingSquare = false
     
     override func didMove(to view: SKView) {
         configScene()
-        recalculateLayout()
+        createGround()
+        createSquare()
     }
     
     override func didChangeSize(_ oldSize: CGSize) {
@@ -34,12 +36,19 @@ class GameScene: SKScene {
     }
     
     private func recalculateLayout() {
-        removeAllChildren()
-        // Recreate the scene objects
-        createGround()
-        createSquare()
+        if let square = square {
+            square.position = CGPoint(x: 0, y: 0)
+        }
+        
+        if let ground = ground {
+            ground.size = CGSize(width: size.width * 2, height: 30)
+            ground.position = CGPoint(x: 0, y: -(size.height / 2) + (ground.size.height / 2))
+            
+            // Update the physics body for the ground after changing its size
+            ground.physicsBody = SKPhysicsBody(rectangleOf: ground.size)
+            ground.physicsBody?.isDynamic = false
+        }
     }
-    
     
     private func createGround(){
         ground = SKSpriteNode(color: SKColor.green, size: CGSize(width: size.width, height: 30))
@@ -59,15 +68,27 @@ class GameScene: SKScene {
     
     // MARK: - Touch Events
     
-    private func touchDown(atPoint pos : CGPoint) {
-        // Not implemented!
+    private func touchDown(atPoint pos: CGPoint) {
+        if square.contains(pos) {
+            // Start dragging the square
+            isDraggingSquare = true
+            square.physicsBody?.isDynamic = false // Disable physics while dragging
+        }
     }
-    
-    private func touchMoved(toPoint pos : CGPoint) {
-        // Not implemented!
+
+    private func touchMoved(toPoint pos: CGPoint) {
+        if isDraggingSquare {
+            // Move the square along with the touch
+            square.position = pos
+        }
     }
-    
-    private func touchUp(atPoint pos : CGPoint) {
+
+    private func touchUp(atPoint pos: CGPoint) {
+        if isDraggingSquare {
+            // End dragging and re-enable physics
+            square.physicsBody?.isDynamic = true // Re-enable physics after release
+            isDraggingSquare = false
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
